c58827898488c17835354fc66b468a15a99008ab
Jonas Niemeyer <jonas.niemeyer@uni-jena.de>
2024-12-20 20:28:43 +0100

    Updates

--- _app/immutable/chunks/paths.taCLGIRj.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_s2aonw)==null?void 0:s.base)??"/weihnachtsr-tsel";var a;const t=((a=globalThis.__sveltekit_s2aonw)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/entry/start.CD1jlXaK.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.jNDoUzd8.js";export{t as start};
